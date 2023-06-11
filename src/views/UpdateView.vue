d01a8594b51f295f252bccb1e4d87f631cf39548
Kowit Wattana <nuiikowit.information@gmail.com>
2023-06-11 10:55:56 +0700

    UPDATE Data

--- src/views/UpdateView.vue
+++ src/views/UpdateView.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="q-pa-md" style="max-width: 400px">
     <q-form @submit="onSubmit" class="q-gutter-md">
-        <q-input v-model="id" label="ID" />
-        <!-- <q-input v-model="fname" label="First Name" />
+        <q-input v-model="id" label="ID" readonly />
+        <q-input v-model="fname" label="First Name" />
         <q-input v-model="lname" label="Last Name" />
         <q-input v-model="username" label="Username" />
         <q-input v-model="password" label="Password" />
         <q-input v-model="email" label="Email" />
-        <q-input v-model="avatar" label="Avatar" /> -->
-        <q-btn label="Submit" type="submit" color="primary" />
+        <q-input v-model="avatar" label="Avatar" />
+        <q-btn label="Update" type="submit" color="primary" />
     </q-form>
 </div>
 </template>
@@ -21,41 +21,59 @@ const route = useRoute();
 
 const id = ref(route.params.id)
 
-// const fname = ref('Cat')
-// const lname = ref('Chat')
-// const username = ref('chat.chat@melivecode.com')
-// const password = ref('1234')
-// const email = ref('chat.chat@melivecode.com')
-// const avatar = ref('https://melivecode.com/users/cat.png')
-// const onSubmit = () => {
-// var myHeaders = new Headers();
-// myHeaders.append("Content-Type", "application/json");
+const fname = ref('')
+const lname = ref('')
+const username = ref('')
+const password = ref('')
+const email = ref('')
+const avatar = ref('')
 
-// var raw = JSON.stringify({
-//     "fname": fname.value,
-//     "lname": lname.value,
-//     "username": username.value,
-//     "password": password.value,
-//     "email": email.value,
-//     "avatar": avatar.value
-// });
+const fetchData = () => {
+    fetch("https://www.melivecode.com/api/users/" + id.value)
+    .then(res => res.json())
+    .then((result) => {
+        // alert(JSON.stringify(result));
+        fname.value = result.user.fname
+        lname.value = result.user.lname
+        username.value = result.user.username
+        password.value = result.user.password
+        email.value = result.user.email
+        avatar.value = result.user.avatar
+    }) 
+}
 
-// var requestOption = {
-//     method: 'POST',
-//     headers: myHeaders,
-//     body: raw,
-//     redirect: 'follow'
-// };
+fetchData()
+
+const onSubmit = () => {
+var myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+var raw = JSON.stringify({
+    "id": id.value,
+    "fname": fname.value,
+    "lname": lname.value,
+    "username": username.value,
+    "password": password.value,
+    "email": email.value,
+    "avatar": avatar.value
+});
+
+var requestOption = {
+    method: 'PUT',
+    headers: myHeaders,
+    body: raw,
+    redirect: 'follow'
+};
 
 
-// fetch("https://www.melivecode.com/api/users/create", requestOption)
-// .then(response => response.json())
-// .then(result => {
-//     alert(result.message)
-//     if (result.status === "ok") {
-//         router.push('/')
-//     }
-// })
-// .catch(error => console.log('error', error));
-// }
+fetch("https://www.melivecode.com/api/users/update", requestOption)
+.then(response => response.json())
+.then(result => {
+    alert(result.message)
+    if (result.status === "ok") {
+         router.push('/')
+    }
+})
+.catch(error => console.log('error', error));
+}
 </script>
